96d2402e541bd7e7fc57041ad2a9061768f1adfc
DerrickF <derrick.w.fink@gmail.com>
2019-03-26 12:19:46 -0600

    Add material themes and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardContainerComponent } from './card-container/card-container.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    BrowserAnimationsModule,
+    MaterialModule
+  ],
+  declarations: [ 
+    AppComponent,
+    CardContainerComponent
+  ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
